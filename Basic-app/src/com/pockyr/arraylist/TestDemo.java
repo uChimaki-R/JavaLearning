1df660d8aa9b0dfed20ab1501c0140f6330d8384
Crestimes <1259864733@qq.com>
2024-05-24 20:57:43 +0800

    添加菜品和展示菜品的小案例，尝试使用容器ArrayList

---
+++ Basic-app/src/com/pockyr/arraylist/TestDemo.java
@@ -0,0 +1,8 @@
+package com.pockyr.arraylist;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        FoodOperator foodOperator = new FoodOperator();
+        foodOperator.start();
+    }
+}
