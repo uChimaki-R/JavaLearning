449be15a7f785160ec853e9e80c5d3d42d718c12
Crestimes <1259864733@qq.com>
2024-05-25 13:56:33 +0800

    尝试继承的使用

---
+++ Basic-app/src/com/pockyr/extendss/TestDemo.java
@@ -0,0 +1,11 @@
+package com.pockyr.extendss;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher();
+        teacher.setName("张三");
+        teacher.setAge(23);
+        teacher.setSkill("Java");
+        teacher.printInfo();
+    }
+}
