77ccf8e746957497455af8013e06043584e73f95
Crestimes <1259864733@qq.com>
2024-05-25 20:36:04 +0800

    子类Teacher类

---
+++ Basic-app/src/com/pockyr/polymorphism/Teacher.java
@@ -0,0 +1,13 @@
+package com.pockyr.polymorphism;
+
+public class Teacher extends Person{
+    public int tid;
+    public Teacher(String name, int age, int tid) {
+        super(name, age);
+        this.tid = tid;
+    }
+    @Override
+    public void sayHello(){
+        System.out.println("Hello, I am Teacher");
+    }
+}
