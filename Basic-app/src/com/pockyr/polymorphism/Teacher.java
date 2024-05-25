9fa49794a5a9739bd7ff74bd6438dd74f8113d71
Crestimes <1259864733@qq.com>
2024-05-25 20:41:35 +0800

    Teacher类独有成员函数setHomework

--- Basic-app/src/com/pockyr/polymorphism/Teacher.java
+++ Basic-app/src/com/pockyr/polymorphism/Teacher.java
@@ -1,13 +1,19 @@
 package com.pockyr.polymorphism;
 
-public class Teacher extends Person{
+public class Teacher extends Person {
     public int tid;
+
     public Teacher(String name, int age, int tid) {
         super(name, age);
         this.tid = tid;
     }
+
     @Override
-    public void sayHello(){
+    public void sayHello() {
         System.out.println("Hello, I am Teacher");
+    }
+
+    public void setHomework() {
+        System.out.println("setting homework...");
     }
 }
