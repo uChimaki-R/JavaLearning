f1d7e6b60aa676ef16e6c7b3ab0d56b2131da893
Crestimes <1259864733@qq.com>
2024-05-25 20:35:51 +0800

    子类Student类

---
+++ Basic-app/src/com/pockyr/polymorphism/Student.java
@@ -0,0 +1,14 @@
+package com.pockyr.polymorphism;
+
+public class Student extends Person {
+    public int sid;
+    public Student() {}
+    public Student(String name, int age, int sid){
+        super(name, age);
+        this.sid = sid;
+    }
+    @Override
+    public void sayHello(){
+        System.out.println("Hello, I am Student");
+    }
+}
