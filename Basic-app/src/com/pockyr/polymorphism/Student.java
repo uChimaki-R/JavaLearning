1dae4277da28093cc682e3480f71fd6fb6caf301
Crestimes <1259864733@qq.com>
2024-05-25 20:41:11 +0800

    Student类独有成员函数doHomework

--- Basic-app/src/com/pockyr/polymorphism/Student.java
+++ Basic-app/src/com/pockyr/polymorphism/Student.java
@@ -2,13 +2,21 @@ package com.pockyr.polymorphism;
 
 public class Student extends Person {
     public int sid;
-    public Student() {}
-    public Student(String name, int age, int sid){
+
+    public Student() {
+    }
+
+    public Student(String name, int age, int sid) {
         super(name, age);
         this.sid = sid;
     }
+
     @Override
-    public void sayHello(){
+    public void sayHello() {
         System.out.println("Hello, I am Student");
+    }
+
+    public void doHomework() {
+        System.out.println("doing homework...");
     }
 }
