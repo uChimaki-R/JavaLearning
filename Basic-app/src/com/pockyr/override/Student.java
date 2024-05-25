8d00f86b9163fee103e1b015f4666d2e57af8d5e
Crestimes <1259864733@qq.com>
2024-05-25 15:46:21 +0800

    使用super显式指定父类的构造方法，使用super指定使用父类的成员函数

--- Basic-app/src/com/pockyr/override/Student.java
+++ Basic-app/src/com/pockyr/override/Student.java
@@ -1,36 +1,43 @@
 package com.pockyr.override;
 
-public class Student {
+public class Student extends People {
     // 所有类都继承Object类
-    private String name;
-    private int age;
+    private float score;
+
+    public float getScore() {
+        return score;
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
 
     public Student() {
     }
 
-    public Student(String name, int age) {
-        this.name = name;
-        this.age = age;
+    public Student(String name, int age, float score) {
+        super(name, age);  // 使用super显式指定父类的构造方法
+        this.score = score;
     }
 
     public String getName() {
-        return name;
+        return super.getName();
     }
 
     public void setName(String name) {
-        this.name = name;
+        super.setName(name);
     }
 
     public int getAge() {
-        return age;
+        return super.getAge();
     }
 
     public void setAge(int age) {
-        this.age = age;
+        super.setAge(age);
     }
 
     @Override  // 使用装饰器会自动检查 更加安全 可读性强
     public String toString() {
-        return "Student [name=" + name + ", age=" + age + "]";
+        return "Student [name=" + getName() + ", age=" + getAge() + "]";
     }
 }
