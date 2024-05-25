4448b4ce973a1303b1e208c1c1085f2c07ad4cbd
Crestimes <1259864733@qq.com>
2024-05-25 20:43:23 +0800

    使用instanceof关键字判断父类指针所指实例的类别并调用对应的子类成员函数

--- Basic-app/src/com/pockyr/polymorphism/TestDemo.java
+++ Basic-app/src/com/pockyr/polymorphism/TestDemo.java
@@ -4,10 +4,16 @@ public class TestDemo {
     public static void main(String[] args) {
         Student student = new Student("st", 7, 111);
         Teacher teacher = new Teacher("te", 50, 999);
-        test(student);
         test(teacher);
+        test(student);
     }
+
     public static void test(Person person) {
         person.sayHello();
+        if (person instanceof Student student) {
+            student.doHomework();
+        } else if (person instanceof Teacher teacher) {
+            teacher.setHomework();
+        }
     }
 }
