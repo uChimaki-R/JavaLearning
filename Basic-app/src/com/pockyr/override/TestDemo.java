33abf1f530a58fce890f0f9240c655d81b917354
Crestimes <1259864733@qq.com>
2024-05-25 15:58:14 +0800

    测试使用不同的参数数量对Student类进行实例化

--- Basic-app/src/com/pockyr/override/TestDemo.java
+++ Basic-app/src/com/pockyr/override/TestDemo.java
@@ -4,5 +4,7 @@ public class TestDemo {
     public static void main(String[] args) {
         Student s = new Student("张三", 12, 90);
         System.out.println(s);
+        Student s2 = new Student("李四", 23);
+        System.out.println(s2);
     }
 }
