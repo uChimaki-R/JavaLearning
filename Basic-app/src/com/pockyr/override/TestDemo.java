7ff4e638d089e694dd0e015a15b3cd8fc9137d0a
Crestimes <1259864733@qq.com>
2024-05-25 15:46:41 +0800

    Student类添加成员变量score

--- Basic-app/src/com/pockyr/override/TestDemo.java
+++ Basic-app/src/com/pockyr/override/TestDemo.java
@@ -2,7 +2,7 @@ package com.pockyr.override;
 
 public class TestDemo {
     public static void main(String[] args) {
-        Student s = new Student("张三", 12);
+        Student s = new Student("张三", 12, 90);
         System.out.println(s);
     }
 }
