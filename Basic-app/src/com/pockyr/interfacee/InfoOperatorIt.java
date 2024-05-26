90fb9d0cc92244779d4f396aaa5468e897a3ce6d
Crestimes <1259864733@qq.com>
2024-05-26 11:07:10 +0800

    管理接口，要求实现打印学生的功能和获取平均数的功能

---
+++ Basic-app/src/com/pockyr/interfacee/InfoOperatorIt.java
@@ -0,0 +1,8 @@
+package com.pockyr.interfacee;
+
+import java.util.ArrayList;
+
+public interface InfoOperatorIt {
+    void printStudents(ArrayList<Student> students);
+    void getAverageScore(ArrayList<Student> students);
+}
