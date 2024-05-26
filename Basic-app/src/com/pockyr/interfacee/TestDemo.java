bc9ed071109856e5db0c583f36dedecc1a663ff6
Crestimes <1259864733@qq.com>
2024-05-26 11:08:57 +0800

    接口的使用小案例

---
+++ Basic-app/src/com/pockyr/interfacee/TestDemo.java
@@ -0,0 +1,9 @@
+package com.pockyr.interfacee;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        ClassManager manager = new ClassManager();
+        manager.printStudents();
+        manager.getAverageScore();
+    }
+}
