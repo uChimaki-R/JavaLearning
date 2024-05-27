f867bb75711e92345895d6e610448284e1c7909f
CodexploRe <1259864733@qq.com>
2024-05-27 15:49:38 +0800

    学生的比较类CompareByAge，实现静态的比较方法

---
+++ Basic-app/src/com/pockyr/arrays/CompareByAge.java
@@ -0,0 +1,7 @@
+package com.pockyr.arrays;
+
+public class CompareByAge {
+    public static int compareByAge(Student s1, Student s2) {
+        return Integer.compare(s1.getAge(), s2.getAge());
+    }
+}
