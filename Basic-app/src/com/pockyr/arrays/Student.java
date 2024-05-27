1552bac1138c686f84109dcefd9772720a2b0903
CodexploRe <1259864733@qq.com>
2024-05-27 10:08:34 +0800

    学生类Student，实现Comparable接口用于数组排序

---
+++ Basic-app/src/com/pockyr/arrays/Student.java
@@ -0,0 +1,44 @@
+package com.pockyr.arrays;
+
+public class Student implements Comparable<Student>{
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        // 重写Comparable接口以实现比较
+        return this.age - o.age;
+    }
+}
