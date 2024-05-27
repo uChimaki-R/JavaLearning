ed421981666ea9ab1a1a8ee74e7f1dbfb78aaa60
CodexploRe <1259864733@qq.com>
2024-05-27 15:54:52 +0800

    CompareByAge类添加非静态方法compareByAgeNotStatic

--- Basic-app/src/com/pockyr/arrays/CompareByAge.java
+++ Basic-app/src/com/pockyr/arrays/CompareByAge.java
@@ -4,4 +4,7 @@ public class CompareByAge {
     public static int compareByAge(Student s1, Student s2) {
         return Integer.compare(s1.getAge(), s2.getAge());
     }
+    public int compareByAgeNotStatic(Student s1, Student s2) {
+        return Integer.compare(s1.getAge(), s2.getAge());
+    }
 }
