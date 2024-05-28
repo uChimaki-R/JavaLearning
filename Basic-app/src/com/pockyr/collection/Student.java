1fb20e3bcb531e7ad8141ac24836267adeaf7142
CodexploRe <1259864733@qq.com>
2024-05-28 08:56:51 +0800

    学生类Student重写hashCode()和equals()方法解决去重问题

---
+++ Basic-app/src/com/pockyr/collection/Student.java
@@ -0,0 +1,54 @@
+package com.pockyr.collection;
+
+import java.util.Objects;
+
+public class Student {
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    // 重写hashCode()和equals()方法解决哈希集合去重问题
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
