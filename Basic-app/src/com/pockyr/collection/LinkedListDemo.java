c0f207307421d3ba6fb0993d57bc5f88b5667b9f
CodexploRe <1259864733@qq.com>
2024-05-28 08:47:24 +0800

    使用LinkedList容器设计栈

--- Basic-app/src/com/pockyr/collection/LinkedListDemo.java
+++ Basic-app/src/com/pockyr/collection/LinkedListDemo.java
@@ -4,7 +4,8 @@ import java.util.LinkedList;
 
 public class LinkedListDemo {
     public static void main(String[] args) {
-        LinkedList<String> list = new LinkedList<String>();
+        // 底层实现是双链表
+        LinkedList<String> list = new LinkedList<>();
         // 设计队列
         list.addLast("A");
         list.addLast("B");
@@ -14,5 +15,16 @@ public class LinkedListDemo {
         System.out.println(list.removeFirst());
         System.out.println(list.removeFirst());
         System.out.println(list.removeFirst());
+
+        list.clear();
+        // 设计栈
+        list.push("G");  // 等价addFirst();
+        list.push("H");
+        list.push("I");
+        list.push("J");
+        System.out.println(list);
+        System.out.println(list.pop());  // 等价removeFirst();
+        System.out.println(list.pop());
+        System.out.println(list.pop());
     }
 }
