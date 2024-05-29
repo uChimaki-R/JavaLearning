17bd5e4a703c1764778f9c26a61dea45bd3af759
CodexploRe <1259864733@qq.com>
2024-05-29 16:31:58 +0800

    使用线程池提交Callable任务并获取结果

--- Basic-app/src/com/pockyr/thread/TestDemo.java
+++ Basic-app/src/com/pockyr/thread/TestDemo.java
@@ -1,24 +1,27 @@
 package com.pockyr.thread;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 public class TestDemo {
     public static void main(String[] args) {
-        // 创建一个实现了Callable接口的类实例
-        Callable<String> callable = new MyTread(100);
-        // 使用FutureTask包装该类实例，使之成为一个任务对象
-        FutureTask<String> futureTask = new FutureTask<>(callable);
-        // 将该任务对象传递到Thread的初始化参数中
-        Thread t1 = new Thread(futureTask);
-        // 启动该任务
-        t1.start();
-        // 使用FutureTask.get方法获取子进程执行结果
-        try {
-            String result = futureTask.get();
-            System.out.println(result);
-        } catch (InterruptedException | ExecutionException e) {
+        try (
+                /*
+                 七个参数
+                 int corePoolSize, // 核心线程数
+                 int maximumPoolSize,  // 最大线程数，当核心线程都在忙且任务队列已满时，可以调用最多maximumPoolSize-corePoolSize个临时线程
+                 long keepAliveTime,  // 临时线程的最大存活时间，和下一个参数组成完整的时间参数
+                 TimeUnit unit,  // keepAliveTime参数的时间单位
+                 BlockingQueue<Runnable> workQueue,  // 任务队列的存储方式，Array是固定长度，Linked链表是Integer.MAX_VALUE长度
+                 ThreadFactory threadFactory,  // 线程工程，决定线程的创建方式，定义线程的名称等
+                 RejectedExecutionHandler handler  // 过多任务的处理方式，Abort丢弃并报错，DiscardOldest替换队列里等了最久的任务，.Discard丢弃且不报错，CallerRuns交给主线程执行
+                 */
+                ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3, 5, 10, TimeUnit.SECONDS,
+                        new ArrayBlockingQueue<>(5), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy())
+        ) {
+            Callable<String> callable = new MyTread(10);
+            Future<String> future = threadPoolExecutor.submit(callable);
+            System.out.println(future.get());
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
