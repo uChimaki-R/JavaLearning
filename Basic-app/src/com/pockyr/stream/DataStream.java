bf1ebe13c13efb307bc7f7b6615280c9b90bb777
Crestimes <1259864733@qq.com>
2024-05-29 11:36:11 +0800

    使用DataInputStream读入基本数据类型

--- Basic-app/src/com/pockyr/stream/DataStream.java
+++ Basic-app/src/com/pockyr/stream/DataStream.java
@@ -5,12 +5,14 @@ import java.io.*;
 public class DataStream {
     public static void main(String[] args) {
         try (
-                DataOutputStream dos = new DataOutputStream(
-                        new BufferedOutputStream(new FileOutputStream("Basic-app\\src\\data.txt")))
+                DataInputStream dis = new DataInputStream(
+                        new BufferedInputStream(new FileInputStream("Basic-app\\src\\data.txt")))
         ) {
-            dos.writeInt(10);
-            dos.writeUTF("Hello World");
-            dos.writeDouble(3.1415);
+            // 读入
+            System.out.println(dis.readInt());
+            System.out.println(dis.readUTF());
+            System.out.println(dis.readDouble());
+            System.out.println(dis.readBoolean());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
