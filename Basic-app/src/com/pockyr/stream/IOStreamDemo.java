c766ad040383a3d89fbd11dbcf7d12d79fff8c02
Crestimes <1259864733@qq.com>
2024-05-29 11:21:05 +0800

    使用InputStreamReader包装FileInputStream解决编码问题

--- Basic-app/src/com/pockyr/stream/IOStreamDemo.java
+++ Basic-app/src/com/pockyr/stream/IOStreamDemo.java
@@ -6,13 +6,13 @@ public class IOStreamDemo {
     public static void main(String[] args) {
         try (
                 InputStream inputStream = new FileInputStream("Basic-app\\src\\data.txt");
-                // 使用缓冲区包装
-                BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+                // 使用InputStreamReader包装解决编码问题
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "GBK");
         ) {
-            byte[] bytes = new byte[1024];
+            char[] chars = new char[1024];
             int len;  // 用len记录读取的长度，不然不足1024字节的地方会打印乱码
-            while((len = bufferedInputStream.read(bytes)) != -1) {
-                System.out.println(new String(bytes, 0, len));
+            while((len = inputStreamReader.read(chars)) != -1) {
+                System.out.println(new String(chars, 0, len));
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
