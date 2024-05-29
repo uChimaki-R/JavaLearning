95145fca0920cdd9e1787637947e3fa46df00184
Crestimes <1259864733@qq.com>
2024-05-29 13:33:08 +0800

    使用匿名内部类简化代码

--- Basic-app/src/com/pockyr/thread/TestDemo.java
+++ Basic-app/src/com/pockyr/thread/TestDemo.java
@@ -2,7 +2,11 @@ package com.pockyr.thread;
 
 public class TestDemo {
     public static void main(String[] args) {
-        Thread t1 = new Thread(new MyTread());
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("子线程输出: " + i);
+            }
+        });
         t1.start();
         for (int i = 0; i < 10; i++) {
             System.out.println("主线程输出: " + i);
