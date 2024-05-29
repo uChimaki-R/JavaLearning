5af42d80ff41c4786acd513d25f4e3518cf76175
Crestimes <1259864733@qq.com>
2024-05-29 11:42:58 +0800

    实现了Serializable接口的学生类

---
+++ Basic-app/src/com/pockyr/stream/Student.java
@@ -0,0 +1,41 @@
+package com.pockyr.stream;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    // 实现Serializable接口标记可以序列化
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
