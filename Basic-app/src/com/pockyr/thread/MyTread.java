a4d6c68638372034073a59be66d1a721a36a4ecc
Crestimes <1259864733@qq.com>
2024-05-29 13:44:38 +0800

    实现Callable接口来自定义线程类，可以返回子线程执行的结果

--- Basic-app/src/com/pockyr/thread/MyTread.java
+++ Basic-app/src/com/pockyr/thread/MyTread.java
@@ -1,10 +1,22 @@
 package com.pockyr.thread;
 
-public class MyTread implements Runnable {
+import java.util.concurrent.Callable;
+
+// Callable接口的泛型指代返回值类型
+public class MyTread implements Callable<String> {
+    // 执行任务中所需的参数在实例化对象的时候传递
+    private final int n;
+
+    public MyTread(int n) {
+        this.n = n;
+    }
+
     @Override
-    public void run() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("子线程输出: " + i);
+    public String call() {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
         }
+        return "result: " + sum;
     }
 }
