6b56799e04d6b07219d8c8039e4977a3bb0712f0
CodexploRe <1259864733@qq.com>
2024-05-29 16:38:34 +0800

    实现Runnable接口的自定义线程类

--- Basic-app/src/com/pockyr/thread/MyTread.java
+++ Basic-app/src/com/pockyr/thread/MyTread.java
@@ -3,20 +3,19 @@ package com.pockyr.thread;
 import java.util.concurrent.Callable;
 
 // Callable接口的泛型指代返回值类型
-public class MyTread implements Callable<String> {
-    // 执行任务中所需的参数在实例化对象的时候传递
-    private final int n;
-
-    public MyTread(int n) {
-        this.n = n;
+public class MyTread implements Runnable {
+    public MyTread() {
     }
 
     @Override
-    public String call() {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += i;
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + "输出: " + i);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
-        return Thread.currentThread().getName() + " got result: " + sum;
     }
 }
