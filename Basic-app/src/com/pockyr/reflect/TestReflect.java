34fb7f83ad00e2c2676d4dbd092722bab0ab5b62
Crestimes <1259864733@qq.com>
2024-05-30 16:57:20 +0800

    测试获取类的构造器

--- Basic-app/src/com/pockyr/reflect/TestReflect.java
+++ Basic-app/src/com/pockyr/reflect/TestReflect.java
@@ -3,6 +3,8 @@ package com.pockyr.reflect;
 
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
+
 public class TestReflect {
     @Test
     public void testGetClass() throws ClassNotFoundException {
@@ -20,5 +22,26 @@ public class TestReflect {
         Class c3 = Class.forName("com.pockyr.reflect.Cat");
         System.out.println(c3.getName());
         System.out.println(c3.getSimpleName());
+    }
+
+    @Test
+    public void testGetConstructor() throws NoSuchMethodException {
+        Class c = Cat.class;
+        // 只能获取public的构造器
+        Constructor[] cs = c.getConstructors();
+        for (Constructor constructor : cs) {
+            System.out.println(constructor.getName() + " --> parameter counts: " + constructor.getParameterCount());
+        }
+        System.out.println("--------------------------------------------");
+        // 使用 getDeclaredConstructors 获取全部构造器，下面的 getDeclaredConstructor 同理
+        Constructor[] cs2 = c.getDeclaredConstructors();
+        for (Constructor constructor : cs2) {
+            System.out.println(constructor.getName() + " --> parameter counts: " + constructor.getParameterCount());
+        }
+
+        System.out.println("--------------------------------------------");
+        // 获取指定的构造器
+        Constructor constructor = c.getDeclaredConstructor(String.class, int.class);
+        System.out.println(constructor.getName() + " --> parameter counts: " + constructor.getParameterCount());
     }
 }
