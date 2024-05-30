499bf215d397c953397e403a5449ab3776753c69
Crestimes <1259864733@qq.com>
2024-05-30 17:03:02 +0800

    测试通过获取的构造器构造实例并忽略构造函数的访问权限检查

--- Basic-app/src/com/pockyr/reflect/TestReflect.java
+++ Basic-app/src/com/pockyr/reflect/TestReflect.java
@@ -4,6 +4,7 @@ package com.pockyr.reflect;
 import org.junit.Test;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class TestReflect {
     @Test
@@ -14,10 +15,10 @@ public class TestReflect {
         System.out.println(c.getName());  // com.pockyr.reflect.Cat
         System.out.println(c.getSimpleName());  // Cat
         // 通过类的实例
-        Cat cat = new Cat("neko", 12);
-        Class c2 = cat.getClass();
-        System.out.println(c2.getName());
-        System.out.println(c2.getSimpleName());
+//        Cat cat = new Cat("neko", 12);
+//        Class c2 = cat.getClass();
+//        System.out.println(c2.getName());
+//        System.out.println(c2.getSimpleName());
         // 通过Class的静态方法，需要传递全类名
         Class c3 = Class.forName("com.pockyr.reflect.Cat");
         System.out.println(c3.getName());
@@ -43,5 +44,16 @@ public class TestReflect {
         // 获取指定的构造器
         Constructor constructor = c.getDeclaredConstructor(String.class, int.class);
         System.out.println(constructor.getName() + " --> parameter counts: " + constructor.getParameterCount());
+
+        // 使用构造器初始化对象
+        try {
+            // 此构造函数在类内是private修饰的，可以通过setAccessible取消检查，强制反射，会破坏类的封装性
+            constructor.setAccessible(true);
+
+            Cat cat = (Cat) constructor.newInstance("neko", 12);
+            System.out.println(cat.getName());
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
