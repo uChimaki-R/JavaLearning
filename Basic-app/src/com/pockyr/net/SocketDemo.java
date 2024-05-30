b984868bb0997ad71a39966b3576ae38cf8a73b3
Crestimes <1259864733@qq.com>
2024-05-30 11:43:44 +0800

    使用循环完成客户端的多次数据发送

--- Basic-app/src/com/pockyr/net/SocketDemo.java
+++ Basic-app/src/com/pockyr/net/SocketDemo.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketDemo {
     public static void main(String[] args) {
@@ -12,13 +13,27 @@ public class SocketDemo {
                 Socket socket = new Socket(InetAddress.getLocalHost(), 8888)
         ) {
             System.out.println("----------ClientSocket started----------");
-            OutputStream out = socket.getOutputStream();
-            // 使用数据流包装
-            DataOutputStream dos = new DataOutputStream(out);
-            dos.writeUTF("Hello World");
-            // 使用flush及时送出数据
-            dos.flush();
-            System.out.println("----------send----------");
+            Scanner scanner = new Scanner(System.in);
+            try (
+                    OutputStream out = socket.getOutputStream();
+                    // 使用数据流包装
+                    DataOutputStream dos = new DataOutputStream(out)
+            ) {
+                while (true) {
+                    System.out.print("input: ");
+                    String message = scanner.nextLine();
+                    if (message.equals("exit")) {
+                        System.out.println("----------exit----------");
+                        break;
+                    }
+                    dos.writeUTF(message);
+                    // 使用flush及时送出数据
+                    dos.flush();
+                    System.out.println("----------send----------");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
