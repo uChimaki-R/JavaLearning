e54883849395f64b44f7ac8da095efdc92873cc4
Crestimes <1259864733@qq.com>
2024-05-30 11:52:21 +0800

    使用多线程完成对多个客户端请求的响应和处理

--- Basic-app/src/com/pockyr/net/ServerSocketDemo.java
+++ Basic-app/src/com/pockyr/net/ServerSocketDemo.java
@@ -1,9 +1,6 @@
 package com.pockyr.net;
 
-import java.io.DataInputStream;
-import java.io.EOFException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,26 +10,11 @@ public class ServerSocketDemo {
                 ServerSocket serverSocket = new ServerSocket(8888)
         ) {
             System.out.println("----------ServerSocket started----------");
-            // 等待连接
-            Socket socket = serverSocket.accept();
-            String address = socket.getInetAddress().getHostAddress();
-            int port = socket.getPort();
-            try (
-                    InputStream in = socket.getInputStream();
-                    DataInputStream dis = new DataInputStream(in)
-            ) {
-                while (true) {
-                    try {
-                        // 客户端主动断开连接时会发生EOFException异常，捕获代表连接断开
-                        String message = dis.readUTF();
-                        System.out.println("received: " + message + " from: " + address + ":" + port);
-                    } catch (EOFException e) {
-                        System.out.println(address + ":" + port + " exit");
-                        break;
-                    }
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            while (true) {
+                // 等待连接
+                Socket socket = serverSocket.accept();
+                // 每个连接单独处理
+                new Thread(new SocketManageThread(socket)).start();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
