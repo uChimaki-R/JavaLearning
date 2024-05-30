6b3b537ce6b479dbf9180fd55807c18a3ab6c6ee
Crestimes <1259864733@qq.com>
2024-05-30 11:44:33 +0800

    使用循环完成服务端的多次数据接收，使用try-catch实现对客户端主动断开连接的检测

--- Basic-app/src/com/pockyr/net/ServerSocketDemo.java
+++ Basic-app/src/com/pockyr/net/ServerSocketDemo.java
@@ -1,6 +1,7 @@
 package com.pockyr.net;
 
 import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
@@ -16,12 +17,23 @@ public class ServerSocketDemo {
             Socket socket = serverSocket.accept();
             String address = socket.getInetAddress().getHostAddress();
             int port = socket.getPort();
-
-            InputStream in = socket.getInputStream();
-            DataInputStream dis = new DataInputStream(in);
-            String message = dis.readUTF();
-
-            System.out.println("received: " + message + " from: " + address + ":" + port);
+            try (
+                    InputStream in = socket.getInputStream();
+                    DataInputStream dis = new DataInputStream(in)
+            ) {
+                while (true) {
+                    try {
+                        // 客户端主动断开连接时会发生EOFException异常，捕获代表连接断开
+                        String message = dis.readUTF();
+                        System.out.println("received: " + message + " from: " + address + ":" + port);
+                    } catch (EOFException e) {
+                        System.out.println(address + ":" + port + " exit");
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
