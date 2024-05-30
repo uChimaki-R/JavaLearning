ebc0766c4a82252d61212fac0d3209f325fd013d
Crestimes <1259864733@qq.com>
2024-05-30 22:24:31 +0800

    添加私有成员方法saySomething

--- Basic-app/src/com/pockyr/reflect/Cat.java
+++ Basic-app/src/com/pockyr/reflect/Cat.java
@@ -28,4 +28,11 @@ public class Cat {
         this.age = age;
     }
 
+    private String saySomething(String something, int times){
+        for(int i = 0; i<times; i++){
+            System.out.println(something);
+        }
+        return something;
+    }
+
 }
