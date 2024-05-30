140ddc4908cdd14e137dc1e0538c795cbb94f67e
Crestimes <1259864733@qq.com>
2024-05-30 15:48:08 +0800

    添加静态变量socket列表，用于管理当前在线的客户端

--- Basic-app/src/com/pockyr/net/ServerSocketDemo.java
+++ Basic-app/src/com/pockyr/net/ServerSocketDemo.java
@@ -3,8 +3,11 @@ package com.pockyr.net;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class ServerSocketDemo {
+    // 存储连接的所有客户端，用于消息的转发
+    public static ArrayList<Socket> sockets = new ArrayList<>();
     public static void main(String[] args) {
         try (
                 ServerSocket serverSocket = new ServerSocket(8888)
@@ -13,6 +16,7 @@ public class ServerSocketDemo {
             while (true) {
                 // 等待连接
                 Socket socket = serverSocket.accept();
+                sockets.add(socket);
                 // 每个连接单独处理
                 new Thread(new SocketManageThread(socket)).start();
             }
