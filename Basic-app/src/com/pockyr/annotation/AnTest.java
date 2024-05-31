1497ac72a279970e0553e4c9ff0284327937f1c8
Crestimes <1259864733@qq.com>
2024-05-31 11:51:13 +0800

    解析注解

--- Basic-app/src/com/pockyr/annotation/AnTest.java
+++ Basic-app/src/com/pockyr/annotation/AnTest.java
@@ -1,8 +1,11 @@
 package com.pockyr.annotation;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class AnTest {
     // 本质是使用参数实例化了接口的类对象
-    @MyAnnotation(value = "hello")
+    @MyAnnotation(value = "hello", id = 999)
     void test1() {
         System.out.println("test1");
     }
@@ -19,5 +22,26 @@ public class AnTest {
 
     void test4() {
         System.out.println("test4");
+    }
+
+    public static void main(String[] args) {
+        // 解析注解
+        // 获取类对象
+        AnTest t = new AnTest();
+        Class c = AnTest.class;
+        // 遍历类内的所有成员方法
+        for (Method method : c.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(MyAnnotation.class)) {
+                // 如果方法有MyAnnotation的注解
+                try {
+                    // 执行该方法
+                    method.invoke(t);
+//                    test1
+//                    test2
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
