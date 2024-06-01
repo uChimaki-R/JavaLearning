ed6f5482dabe67b8040772295c7d60e423431164
Crestimes <1259864733@qq.com>
2024-06-01 15:51:54 +0800

    js输出语句

--- WebBase/FrontBase/Base2/js/demo.js
+++ WebBase/FrontBase/Base2/js/demo.js
@@ -1,1 +1,9 @@
-alert('Hei! Man! What can I say!')
+// 三种输出方式
+// 1、弹出警告框
+window.alert('Hei! Man! What can I say!');
+// 2、写入html页面
+document.write("Wrote in document.");
+// 3、在浏览器控制台输出
+console.log("A log to console.");
+
+// 行尾的分号可以不写，但是最好写上
