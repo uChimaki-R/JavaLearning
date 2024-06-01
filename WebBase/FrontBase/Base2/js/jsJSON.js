64b3c88f54a98f2257175d6ee86d0e9b86cd5bb8
Crestimes <1259864733@qq.com>
2024-06-01 20:28:49 +0800

    JSON对象

--- WebBase/FrontBase/Base2/js/jsJSON.js
+++ WebBase/FrontBase/Base2/js/jsJSON.js
@@ -14,3 +14,13 @@ var user = {
 
 document.writeln(user.name);
 user.eat();
+
+var jsonStr = '{"name": "Tom", "age": 18, "skill": ["Python", "Java", "C++"]}';
+
+// 解析json数据为json对象
+var jsonObj = JSON.parse(jsonStr);
+document.writeln(jsonObj.name);
+
+// json对象转json格式字符串
+var toJson = JSON.stringify(jsonObj);
+document.writeln(toJson);
