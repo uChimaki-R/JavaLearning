32fc8d27d0af36855015084ea5f495b5e178b0ca
Crestimes <1259864733@qq.com>
2024-06-01 15:59:42 +0800

    js代码变量的定义

--- WebBase/FrontBase/Base2/js/demo.js
+++ WebBase/FrontBase/Base2/js/demo.js
@@ -7,3 +7,25 @@ document.write("Wrote in document.");
 console.log("A log to console.");
 
 // 行尾的分号可以不写，但是最好写上
+
+var a = 100;
+a = "js代码是弱数据类型的代码, 同个变量可以接收不同类型的变量";
+
+// var的作用域大，产生的变量是全局变量
+{
+    var x = 50;
+}
+alert(x); // 可以访问
+
+// var定义的变量可以重复声明
+var x = 12;
+var x = "hi";
+
+// let定义的变量为局部变量且不可重复定义
+{
+    // let x = 10;
+    // let x = "hi";
+}
+
+// const声明常量
+const pi = 3.1416
