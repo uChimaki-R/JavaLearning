67349c687c8c59028407c2a4e09ae67af90f2fed
Crestimes <1259864733@qq.com>
2024-06-01 16:14:05 +0800

    js数据类型和运算符、数据类型的转换、流程控制语句

--- WebBase/FrontBase/Base2/js/demo.js
+++ WebBase/FrontBase/Base2/js/demo.js
@@ -2,7 +2,7 @@
 // 1、弹出警告框
 window.alert('Hei! Man! What can I say!');
 // 2、写入html页面
-document.write("Wrote in document.");
+document.writeln("Wrote in document.");
 // 3、在浏览器控制台输出
 console.log("A log to console.");
 
@@ -29,3 +29,46 @@ var x = "hi";
 
 // const声明常量
 const pi = 3.1416
+
+// js的原始数据类型
+// number
+document.writeln(typeof 3);
+document.writeln(typeof 3.14);
+// string
+document.writeln(typeof "A");
+document.writeln(typeof 'hi');
+// boolean
+document.writeln(typeof true);
+document.writeln(typeof false);
+// object
+document.writeln(typeof null);
+// undefined
+var aaaa;
+document.writeln(typeof aaaa);
+
+// 比较运算符基本一致
+// 特殊的 == 和 ===
+// == 比较前会进行类型转换 而 === 不会
+var a = 10;
+document.writeln(a == "10");  // true
+document.writeln(a === "10");  // false
+document.writeln(a === 10);  // true
+
+
+// 类型转换
+
+// 字符串转数字
+document.writeln(parseInt("101"));
+// 从头开始转换直到不是数字
+document.writeln(parseInt("101hi"));  // 101
+document.writeln(parseInt("a110"));  // NaN
+
+// 数字转boolean
+// 除了0和NaN 其他都是true
+
+// 字符串转boolean
+// 除了空串 其他都是true
+
+// null和undefined都是false
+
+// 流程控制语句基本一致
