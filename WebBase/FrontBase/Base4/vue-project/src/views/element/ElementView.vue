5f0d667c02f89f990472638921e9517d7fd7898f
Crestimes <1259864733@qq.com>
2024-06-10 18:35:02 +0800

    Dialog组件

--- WebBase/FrontBase/Base4/vue-project/src/views/element/ElementView.vue
+++ WebBase/FrontBase/Base4/vue-project/src/views/element/ElementView.vue
@@ -32,6 +32,30 @@
       :total="1000"
     >
     </el-pagination>
+
+    <br />
+
+    <!-- dialog对话框 -->
+    <!-- Table -->
+    <el-button type="text" @click="dialogTableVisible = true"
+      >打开嵌套表格的 Dialog</el-button
+    >
+
+    <el-dialog title="收货地址" :visible.sync="dialogTableVisible">
+      <el-table :data="gridData">
+        <el-table-column
+          property="date"
+          label="日期"
+          width="150"
+        ></el-table-column>
+        <el-table-column
+          property="name"
+          label="姓名"
+          width="200"
+        ></el-table-column>
+        <el-table-column property="address" label="地址"></el-table-column>
+      </el-table>
+    </el-dialog>
   </div>
 </template>
 
@@ -39,6 +63,29 @@
 export default {
   data() {
     return {
+      gridData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+      ],
+      dialogTableVisible: false,
       tableData: [
         {
           date: "2016-05-02",
