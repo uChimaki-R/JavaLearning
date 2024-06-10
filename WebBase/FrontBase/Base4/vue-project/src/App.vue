10a6c04ba819651c90bd668430a5c7d94581a5af
Crestimes <1259864733@qq.com>
2024-06-10 16:26:52 +0800

    在App.vue中导入自定义的组件

--- WebBase/FrontBase/Base4/vue-project/src/App.vue
+++ WebBase/FrontBase/Base4/vue-project/src/App.vue
@@ -1,14 +1,18 @@
 <!-- 定义html代码 -->
 <template>
   <div>
-    <h1>{{ message }}</h1>
+    <element-view>
+      
+    </element-view>
   </div>
 </template>
 
 <!-- 定义js代码，定义vue的数据模型和方法 -->
 <script>
+import ElementView from './views/element/ElementView.vue';
 // export后才能在其他js文件中导入
 export default {
+  components: { ElementView },
   // 数据模型定义的不是对象，而是返回对象的函数
   data() {
     return {
