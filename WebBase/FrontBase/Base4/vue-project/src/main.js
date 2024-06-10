bc41082bb1d276447ebd60f46b929928a51bc804
Crestimes <1259864733@qq.com>
2024-06-10 16:26:47 +0800

    main.js引入element-ui包

--- WebBase/FrontBase/Base4/vue-project/src/main.js
+++ WebBase/FrontBase/Base4/vue-project/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
+// 使用ElementUI
+Vue.use(ElementUI);
 
 new Vue({
   router,
