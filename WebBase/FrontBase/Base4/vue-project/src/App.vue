85a66ab8252afa172bb71e0df7b74e3b8d88b63f
Crestimes <1259864733@qq.com>
2024-06-11 00:13:59 +0800

    导入并展示案例界面

--- WebBase/FrontBase/Base4/vue-project/src/App.vue
+++ WebBase/FrontBase/Base4/vue-project/src/App.vue
@@ -1,18 +1,18 @@
 <!-- 定义html代码 -->
 <template>
   <div>
-    <element-view>
-      
-    </element-view>
+    <!-- <element-view></element-view> -->
+    <emp-view></emp-view>
   </div>
 </template>
 
 <!-- 定义js代码，定义vue的数据模型和方法 -->
 <script>
-import ElementView from './views/element/ElementView.vue';
+// import ElementView from './views/element/ElementView.vue';
+import EmpView from './views/myDemo/EmpView.vue';
 // export后才能在其他js文件中导入
 export default {
-  components: { ElementView },
+  components: { /* ElementView */ EmpView },
   // 数据模型定义的不是对象，而是返回对象的函数
   data() {
     return {
