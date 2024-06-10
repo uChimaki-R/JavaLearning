a6a6d346b21b3cbda54848eb1fc7f5561d998a03
Crestimes <1259864733@qq.com>
2024-06-10 18:47:03 +0800

    Form表单组件

--- WebBase/FrontBase/Base4/vue-project/src/views/element/ElementView.vue
+++ WebBase/FrontBase/Base4/vue-project/src/views/element/ElementView.vue
@@ -36,7 +36,6 @@
     <br />
 
     <!-- dialog对话框 -->
-    <!-- Table -->
     <el-button type="text" @click="dialogTableVisible = true"
       >打开嵌套表格的 Dialog</el-button
     >
@@ -56,6 +55,49 @@
         <el-table-column property="address" label="地址"></el-table-column>
       </el-table>
     </el-dialog>
+
+    <br />
+
+    <!-- dialog对话框嵌套Form表单 -->
+    <el-button type="text" @click="dialogFormVisible = true"
+      >打开嵌套Form表单的 Dialog</el-button
+    >
+    <el-dialog title="Form表单" :visible.sync="dialogFormVisible">
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="活动名称">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="活动时间">
+          <el-col :span="11">
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              v-model="form.date1"
+              style="width: 100%"
+            ></el-date-picker>
+          </el-col>
+          <el-col class="line" :span="2">-</el-col>
+          <el-col :span="11">
+            <el-time-picker
+              placeholder="选择时间"
+              v-model="form.date2"
+              style="width: 100%"
+            ></el-time-picker>
+          </el-col>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">立即创建</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -63,6 +105,12 @@
 export default {
   data() {
     return {
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+      },
       gridData: [
         {
           date: "2016-05-02",
@@ -86,6 +134,7 @@ export default {
         },
       ],
       dialogTableVisible: false,
+      dialogFormVisible: false,
       tableData: [
         {
           date: "2016-05-02",
@@ -128,6 +177,12 @@ export default {
       }
       return "";
     },
+
+    // 点击提交Form表单内容后可以通过这个方法用axios的post传给后端
+    onSubmit(){
+      // 需要使用this获取data中的数据
+      console.log(JSON.stringify(this.form));
+    }
   },
 };
 </script>
