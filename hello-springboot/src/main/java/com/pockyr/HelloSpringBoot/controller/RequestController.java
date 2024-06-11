bd5e2f6132b3ea744ed0cce4f94caaa9584a9e0d
Crestimes <1259864733@qq.com>
2024-06-11 23:57:34 +0800

    使用简单实体类对象接收请求参数数据

--- hello-springboot/src/main/java/com/pockyr/HelloSpringBoot/controller/RequestController.java
+++ hello-springboot/src/main/java/com/pockyr/HelloSpringBoot/controller/RequestController.java
@@ -1,14 +1,32 @@
 package com.pockyr.HelloSpringBoot.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletRequest;
+
+import com.pockyr.HelloSpringBoot.pojo.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RequestController {
+//    @RequestMapping("/params")
+//    public String GetSomeParams(HttpServletRequest request) {
+//        String name = request.getParameter("name");
+//        String strAge = request.getParameter("age");
+//        // HttpServletRequest获取的参数都是String类型，需要手动转换类型
+//        int age = Integer.parseInt(strAge);
+//        System.out.println(name + ": " + age);
+//        return "OK";
+//    }
+
     @RequestMapping("/params")
     public String GetSomeParams(String name, Integer age) {
         System.out.println(name + ": " + age);
+        return "OK";
+    }
+
+    @RequestMapping("/pojoParams")
+    public String GetSomePojoParams(User user) {
+        System.out.println(user);
         return "OK";
     }
 }
