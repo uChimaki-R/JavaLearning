ef5627bb87ea392b4f671761279e5b70b69aed72
Crestimes <1259864733@qq.com>
2024-06-11 22:14:15 +0800

    spring boot项目文件

---
+++ hello-springboot/src/test/java/com/pockyr/HelloSpringBoot/HelloSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pockyr.HelloSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
