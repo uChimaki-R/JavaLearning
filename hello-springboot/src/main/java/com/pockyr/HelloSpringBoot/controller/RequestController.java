097c378675615c55f4193febaec54be0a4165142
Crestimes <1259864733@qq.com>
2024-06-11 23:43:19 +0800

    使用同名参数解析请求参数数据，不用类型转换

--- hello-springboot/src/main/java/com/pockyr/HelloSpringBoot/controller/RequestController.java
+++ hello-springboot/src/main/java/com/pockyr/HelloSpringBoot/controller/RequestController.java
@@ -7,11 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RequestController {
     @RequestMapping("/params")
-    public String GetSomeParams(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String strAge = request.getParameter("age");
-        // HttpServletRequest获取的参数都是String类型，需要手动转换类型
-        int age = Integer.parseInt(strAge);
+    public String GetSomeParams(String name, Integer age) {
         System.out.println(name + ": " + age);
         return "OK";
     }
