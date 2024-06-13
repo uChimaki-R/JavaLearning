f55706d94444780da5bd018b940dd287c43402f9
Crestimes <1259864733@qq.com>
2024-06-13 09:47:50 +0800

    使用user-view

--- WebBase/FrontBase/Base4/vue-project/src/App.vue
+++ WebBase/FrontBase/Base4/vue-project/src/App.vue
@@ -2,17 +2,18 @@
 <template>
   <div>
     <!-- <element-view></element-view> -->
-    <emp-view></emp-view>
+    <!-- <emp-view></emp-view> -->
+    <user-view></user-view>
   </div>
 </template>
 
 <!-- 定义js代码，定义vue的数据模型和方法 -->
 <script>
 // import ElementView from './views/element/ElementView.vue';
-import EmpView from './views/myDemo/EmpView.vue';
+import UserView from './views/myTestElement/UserView.vue';
 // export后才能在其他js文件中导入
 export default {
-  components: { /* ElementView */ EmpView },
+  components: { /* ElementView */ /* EmpView, */ UserView },
   // 数据模型定义的不是对象，而是返回对象的函数
   data() {
     return {
