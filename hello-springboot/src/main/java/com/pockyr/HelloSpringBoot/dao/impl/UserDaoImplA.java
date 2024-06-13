cc68d75c53be9131fa88ea015626adbd12f1e6e3
Crestimes <1259864733@qq.com>
2024-06-13 10:07:43 +0800

    将自生交给容器处理用于依赖注入DI

--- hello-springboot/src/main/java/com/pockyr/HelloSpringBoot/dao/impl/UserDaoImplA.java
+++ hello-springboot/src/main/java/com/pockyr/HelloSpringBoot/dao/impl/UserDaoImplA.java
@@ -3,10 +3,12 @@ package com.pockyr.HelloSpringBoot.dao.impl;
 import com.pockyr.HelloSpringBoot.dao.UserDaoInterface;
 import com.pockyr.HelloSpringBoot.pojo.User;
 import com.pockyr.HelloSpringBoot.utils.XMLParserUtil;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Objects;
 
+@Component
 public class UserDaoImplA implements UserDaoInterface {
     @Override
     public List<User> getUsers() {
