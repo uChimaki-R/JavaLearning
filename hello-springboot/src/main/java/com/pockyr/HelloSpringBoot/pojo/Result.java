f3eeb3ae2e708a9bf79e28695db490d70cdbe174
Crestimes <1259864733@qq.com>
2024-06-13 09:51:33 +0800

    用于统一响应数据格式的响应数据类

---
+++ hello-springboot/src/main/java/com/pockyr/HelloSpringBoot/pojo/Result.java
@@ -0,0 +1,54 @@
+package com.pockyr.HelloSpringBoot.pojo;
+
+public class Result {
+    private int code;
+    private String msg;
+    private Object data;
+
+    public static Result success(Object data) {
+        return new Result(200, "success", data);
+    }
+
+    public static Result fail(int code, String msg) {
+        return new Result(code, msg, null);
+    }
+
+    public Result(int code, String msg, Object data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
