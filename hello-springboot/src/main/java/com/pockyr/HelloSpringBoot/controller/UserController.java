f5d579619000f96d563a9ed382d2ef6073e13b37
Crestimes <1259864733@qq.com>
2024-06-13 09:54:03 +0800

    处理请求和响应请求的controller类

---
+++ hello-springboot/src/main/java/com/pockyr/HelloSpringBoot/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.pockyr.HelloSpringBoot.controller;
+
+import com.pockyr.HelloSpringBoot.pojo.Result;
+import com.pockyr.HelloSpringBoot.pojo.User;
+import com.pockyr.HelloSpringBoot.service.UserServiceInterface;
+import com.pockyr.HelloSpringBoot.service.impl.UserServiceImplA;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+// 等价下面两个注解
+// @ResponseBody 注解在类上代表类中的方法返回值都会被包装为body中的数据
+// @Component
+public class UserController {
+    @RequestMapping("/getUserData")
+    public Result getUserData() {
+        UserServiceInterface userService = new UserServiceImplA();
+        List<User> users = userService.getFormattedUsers();
+        return Result.success(users);
+    }
+}
