e1f3933dcfb335e856e6770e3af910c2fc9719c4
Crestimes <1259864733@qq.com>
2024-06-13 10:16:01 +0800

    dao部分的IOC注解使用Repository更为准确易读

--- hello-springboot/src/main/java/com/pockyr/HelloSpringBoot/dao/impl/UserDaoImplA.java
+++ hello-springboot/src/main/java/com/pockyr/HelloSpringBoot/dao/impl/UserDaoImplA.java
@@ -3,12 +3,12 @@ package com.pockyr.HelloSpringBoot.dao.impl;
 import com.pockyr.HelloSpringBoot.dao.UserDaoInterface;
 import com.pockyr.HelloSpringBoot.pojo.User;
 import com.pockyr.HelloSpringBoot.utils.XMLParserUtil;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Objects;
 
-@Component
+@Repository
 public class UserDaoImplA implements UserDaoInterface {
     @Override
     public List<User> getUsers() {
