422d0283bef211d9a4b87b01702978ce627ab1a0
Crestimes <1259864733@qq.com>
2024-06-13 09:46:18 +0800

    简单的vue界面，用于测试异步请求

---
+++ WebBase/FrontBase/Base4/vue-project/src/views/myTestElement/UserView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <el-button type="primary" @click="sendRequest">update</el-button>
+    <br /><br />
+    <span>Message: {{ UserData }}</span>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      UserData: [],
+    };
+  },
+  methods: {
+    sendRequest() {
+      axios
+        .get("/getUserData")
+        .then((result) => (this.UserData = result.data.data));
+    },
+  },
+  updated() {
+    const h = this.$createElement;
+    this.$notify({
+      title: "获取到响应信息",
+      message: h("i", { style: "color: teal" }, "快去查看吧~"),
+    });
+  },
+};
+</script>
+
+<style>
+</style>
