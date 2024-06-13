9f879f5338e67d9cd1ca7b03449e38ff16ecbf3c
Crestimes <1259864733@qq.com>
2024-06-13 10:07:10 +0800

    使用IOC-DI的方法与UserServiceInterface的具体实现类解耦

--- hello-springboot/src/main/java/com/pockyr/HelloSpringBoot/controller/UserController.java
+++ hello-springboot/src/main/java/com/pockyr/HelloSpringBoot/controller/UserController.java
@@ -4,6 +4,7 @@ import com.pockyr.HelloSpringBoot.pojo.Result;
 import com.pockyr.HelloSpringBoot.pojo.User;
 import com.pockyr.HelloSpringBoot.service.UserServiceInterface;
 import com.pockyr.HelloSpringBoot.service.impl.UserServiceImplA;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,10 @@ import java.util.List;
 // @ResponseBody 注解在类上代表类中的方法返回值都会被包装为body中的数据
 // @Component
 public class UserController {
+    @Autowired
+    private UserServiceInterface userService;
     @RequestMapping("/getUserData")
     public Result getUserData() {
-        UserServiceInterface userService = new UserServiceImplA();
         List<User> users = userService.getFormattedUsers();
         return Result.success(users);
     }
