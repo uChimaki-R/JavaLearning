f4dd8829f4a6cf53cdb7139ee69e00b3f7dd23dc
Crestimes <1259864733@qq.com>
2024-06-13 19:03:01 +0800

    init project

---
+++ hello-spring-mybatis/src/main/java/com/pockyr/HelloSpringMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.pockyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringMybatisApplication.class, args);
+    }
+
+}
