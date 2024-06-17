b23060d6074954e73fc46eb797cdc9573debf7eb
CodexploRe <1259864733@qq.com>
2024-06-17 15:58:20 +0800

    统一的返回数据包装类

---
+++ spring-demo/src/main/java/com/pockyr/pojo/Result.java
@@ -0,0 +1,26 @@
+package com.pockyr.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Result {
+    private int code;
+    private String msg;
+    private Object data;
+
+    public static Result success(Object data) {
+        return new Result(1, "success", data);
+    }
+
+    public static Result success() {
+        return new Result(1, "success", null);
+    }
+
+    public static Result error() {
+        return new Result(0, "error", null);
+    }
+}
