eec08e5af6c200e01232a14a54d5ed45b05a8753
CodexploRe <1259864733@qq.com>
2024-06-17 16:02:04 +0800

    部门数据处理方法接口的实现类，getDepartments方法通过私有变量deptMapper获取数据全部部门数据(并处理)后返回

---
+++ spring-demo/src/main/java/com/pockyr/service/impl/DeptService.java
@@ -0,0 +1,20 @@
+package com.pockyr.service.impl;
+
+import com.pockyr.mapper.DeptMapperInterface;
+import com.pockyr.pojo.Department;
+import com.pockyr.service.DeptServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeptService implements DeptServiceInterface {
+    @Autowired
+    private DeptMapperInterface deptMapper;
+
+    @Override
+    public List<Department> getDepartments() {
+        return deptMapper.getDepartments();
+    }
+}
