632591994fc86251bd75277ed4d04f02d36f1685
CodexploRe <1259864733@qq.com>
2024-06-17 15:59:14 +0800

    员工数据处理方法接口的实现类

---
+++ spring-demo/src/main/java/com/pockyr/service/impl/EmpService.java
@@ -0,0 +1,8 @@
+package com.pockyr.service.impl;
+
+import com.pockyr.service.EmpServiceInterface;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmpService implements EmpServiceInterface {
+}
