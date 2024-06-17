5834802e6c1321077efd8a9b9f6ec948f9b2d617
CodexploRe <1259864733@qq.com>
2024-06-17 16:49:09 +0800

    添加处理添加一个新的部门的请求的方法，修改部分方法名称，添加日志输出

--- spring-demo/src/main/java/com/pockyr/controller/DeptController.java
+++ spring-demo/src/main/java/com/pockyr/controller/DeptController.java
@@ -3,14 +3,13 @@ package com.pockyr.controller;
 import com.pockyr.pojo.Department;
 import com.pockyr.pojo.Result;
 import com.pockyr.service.impl.DeptService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class DeptController {
     @Autowired
@@ -21,8 +20,9 @@ public class DeptController {
      * @return 响应信息
      */
     @GetMapping("/depts")
-    public Result getDepartments() {
+    public Result getAll() {
         List<Department> departments = deptService.getAll();
+        log.debug("获取全部部门信息, 总数: {}", departments.size());
         return Result.success(departments);
     }
 
@@ -32,8 +32,21 @@ public class DeptController {
      * @return 响应信息
      */
     @DeleteMapping("/depts/{id}")
-    public Result deleteDepartments(@PathVariable Integer id) {
+    public Result deleteById(@PathVariable Integer id) {
         deptService.deleteById(id);
+        log.info("删除部门, 部门id: {}", id);
+        return Result.success();
+    }
+
+    /**
+     * 添加一个新的部门
+     * @param department 部门信息(仅在body传递部门的名称)
+     * @return 响应信息
+     */
+    @PostMapping("/depts")
+    public Result add(@RequestBody Department department){
+        deptService.add(department);
+        log.info("添加部门: {}", department);
         return Result.success();
     }
 }
