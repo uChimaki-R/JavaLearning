68fc07adbe22d37391c9d672b170e29ef39b48d6
CodexploRe <1259864733@qq.com>
2024-06-17 16:18:49 +0800

    添加处理根据部门id删除指定部门信息的请求的方法，添加函数注释文档

--- spring-demo/src/main/java/com/pockyr/controller/DeptController.java
+++ spring-demo/src/main/java/com/pockyr/controller/DeptController.java
@@ -4,7 +4,9 @@ import com.pockyr.pojo.Department;
 import com.pockyr.pojo.Result;
 import com.pockyr.service.impl.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,9 +16,24 @@ public class DeptController {
     @Autowired
     private DeptService deptService;
 
+    /**
+     * 获取全部部门信息
+     * @return 响应信息
+     */
     @GetMapping("/depts")
     public Result getDepartments() {
         List<Department> departments = deptService.getDepartments();
         return Result.success(departments);
+    }
+
+    /**
+     * 根据部门id删除指定部门信息
+     * @param id 部门id
+     * @return 响应信息
+     */
+    @DeleteMapping("/depts/{id}")
+    public Result deleteDepartments(@PathVariable Integer id) {
+        deptService.deleteDepartments(id);
+        return Result.success();
     }
 }
