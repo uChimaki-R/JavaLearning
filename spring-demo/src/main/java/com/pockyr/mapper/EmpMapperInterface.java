71aea62f0061d5c337be1d329d2bf7ad43f814da
Crestimes <1259864733@qq.com>
2024-06-18 21:10:17 +0800

    添加获取数据总数和获取指定页数页面的数据的sql语句

--- spring-demo/src/main/java/com/pockyr/mapper/EmpMapperInterface.java
+++ spring-demo/src/main/java/com/pockyr/mapper/EmpMapperInterface.java
@@ -1,4 +1,16 @@
 package com.pockyr.mapper;
 
+import com.pockyr.pojo.Employee;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
 public interface EmpMapperInterface {
+    @Select("select count(*) from emp;")
+    Long count();
+
+    @Select("select * from emp limit #{start}, #{pageSize}")
+    List<Employee> page(Integer start, Integer pageSize);
 }
