054327b5ebab841278de97c0c0556519e06360c5
Crestimes <1259864733@qq.com>
2024-06-18 22:31:36 +0800

    添加delete方法

--- spring-demo/src/main/java/com/pockyr/service/EmpServiceInterface.java
+++ spring-demo/src/main/java/com/pockyr/service/EmpServiceInterface.java
@@ -3,7 +3,10 @@ package com.pockyr.service;
 import com.pockyr.pojo.PageBean;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface EmpServiceInterface {
     PageBean page(Integer page, Integer pageSize, String namePart, Short gender, LocalDate begin, LocalDate end);
+
+    void delete(List<Integer> ids);
 }
