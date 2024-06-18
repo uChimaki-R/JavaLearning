0d97dde79b5ce4019735bd5c2ce2ecad8aaf0533
Crestimes <1259864733@qq.com>
2024-06-18 21:35:14 +0800

    使用PageHelper插件优化page方法

--- spring-demo/src/main/java/com/pockyr/service/impl/EmpService.java
+++ spring-demo/src/main/java/com/pockyr/service/impl/EmpService.java
@@ -1,5 +1,7 @@
 package com.pockyr.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.pockyr.mapper.EmpMapperInterface;
 import com.pockyr.pojo.Employee;
 import com.pockyr.pojo.PageBean;
@@ -16,14 +18,22 @@ public class EmpService implements EmpServiceInterface {
 
     @Override
     public PageBean page(Integer page, Integer pageSize) {
-        // 先获取总数
-        Long count = empMapper.count();
+        // 手动分页
+//        // 先获取总数
+//        Long count = empMapper.count();
+//
+//        // 再根据limit获取指定数据
+//        Integer start = (page - 1) * pageSize;
+//        List<Employee> employees = empMapper.page(start, pageSize);
 
-        // 再根据limit获取指定数据
-        Integer start = (page - 1) * pageSize;
-        List<Employee> employees = empMapper.page(start, pageSize);
+        // 使用插件分页
+        // 设置分页参数
+        PageHelper.startPage(page, pageSize);
+        // 执行查询语句, 该语句会被PageHelper拦截并替换为手动分页相同的语句
+        List<Employee> employees = empMapper.selectAll();
+        Page<Employee> empPage = (Page<Employee>) employees; // 转换为Page<>获取相关信息
 
         // 将总数和页面的数据组装返回
-        return new PageBean(count, employees);
+        return new PageBean(empPage.getTotal(), empPage.getResult());
     }
 }
