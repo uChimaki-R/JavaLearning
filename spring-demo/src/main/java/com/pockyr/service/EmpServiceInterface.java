be6a31239ce0e73fb926fb058e153a6e0d3e78c7
Crestimes <1259864733@qq.com>
2024-06-18 22:15:28 +0800

    完善page方法的参数

--- spring-demo/src/main/java/com/pockyr/service/EmpServiceInterface.java
+++ spring-demo/src/main/java/com/pockyr/service/EmpServiceInterface.java
@@ -2,6 +2,8 @@ package com.pockyr.service;
 
 import com.pockyr.pojo.PageBean;
 
+import java.time.LocalDate;
+
 public interface EmpServiceInterface {
-    PageBean page(Integer page, Integer pageSize);
+    PageBean page(Integer page, Integer pageSize, String namePart, Short gender, LocalDate begin, LocalDate end);
 }
