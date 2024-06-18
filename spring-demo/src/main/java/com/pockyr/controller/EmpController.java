801510e5cad759df6ed7b6f1d94056ec76266608
Crestimes <1259864733@qq.com>
2024-06-18 22:10:17 +0800

    完善page方法，添加筛选数据的参数传递

--- spring-demo/src/main/java/com/pockyr/controller/EmpController.java
+++ spring-demo/src/main/java/com/pockyr/controller/EmpController.java
@@ -4,9 +4,12 @@ import com.pockyr.pojo.PageBean;
 import com.pockyr.pojo.Result;
 import com.pockyr.service.impl.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 public class EmpController {
@@ -14,14 +17,17 @@ public class EmpController {
     private EmpService empService;
 
     /**
-     * 获取指定页数中的Employee数据和数据总数total, 包装为实体类PageBean, 传递到响应信息中
-     * @param page 页数
+     * 获取通过条件筛选出来的Employees数据中指定页数中的Employee数据和数据总数total, 包装为实体类PageBean, 传递到响应信息中
+     *
+     * @param page     页数
      * @param pageSize 一页的数据量
      * @return 响应信息
      */
     @GetMapping("/emps")
-    public Result page(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer pageSize) {
-        PageBean pageBean = empService.page(page, pageSize);
+    public Result page(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer pageSize,
+                       String name, Short gender, @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+                       @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
+        PageBean pageBean = empService.page(page, pageSize, name, gender, begin, end);
         return Result.success(pageBean);
     }
 }
