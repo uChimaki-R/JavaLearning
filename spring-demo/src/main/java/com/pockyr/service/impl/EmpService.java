e398d1651da7b81e9de15b3d0506fe704442ed60
Crestimes <1259864733@qq.com>
2024-06-18 22:13:54 +0800

    修正部分代码，添加和修正部分注释。重命名接口: EmpMapperInterface -> EmpMapper（因为mapper无需自己手写实现类，命名就简化掉Interface了）

--- spring-demo/src/main/java/com/pockyr/service/impl/EmpService.java
+++ spring-demo/src/main/java/com/pockyr/service/impl/EmpService.java
@@ -2,22 +2,23 @@ package com.pockyr.service.impl;
 
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
-import com.pockyr.mapper.EmpMapperInterface;
+import com.pockyr.mapper.EmpMapper;
 import com.pockyr.pojo.Employee;
 import com.pockyr.pojo.PageBean;
 import com.pockyr.service.EmpServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class EmpService implements EmpServiceInterface {
     @Autowired
-    private EmpMapperInterface empMapper;
+    private EmpMapper empMapper;
 
     @Override
-    public PageBean page(Integer page, Integer pageSize) {
+    public PageBean page(Integer page, Integer pageSize, String namePart, Short gender, LocalDate begin, LocalDate end) {
         // 手动分页
 //        // 先获取总数
 //        Long count = empMapper.count();
@@ -29,11 +30,12 @@ public class EmpService implements EmpServiceInterface {
         // 使用插件分页
         // 设置分页参数
         PageHelper.startPage(page, pageSize);
-        // 执行查询语句, 该语句会被PageHelper拦截并替换为手动分页相同的语句
-        List<Employee> employees = empMapper.selectAll();
-        Page<Employee> empPage = (Page<Employee>) employees; // 转换为Page<>获取相关信息
+        // 执行查询语句
+        List<Employee> employees = empMapper.selectByConditions(namePart, gender, begin, end);
+        // 将结果包装为Page<>
+        Page<Employee> empPage = (Page<Employee>) employees;
 
-        // 将总数和页面的数据组装返回
+        // 使用Page的方法获取信息
         return new PageBean(empPage.getTotal(), empPage.getResult());
     }
 }
