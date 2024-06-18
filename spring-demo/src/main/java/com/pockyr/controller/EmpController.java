6164ed9d9d94e66f2a139cc1e305d6ac25b41f00
Crestimes <1259864733@qq.com>
2024-06-18 22:52:27 +0800

    添加处理新增员工的请求的方法，简化注解中的请求路径，修改部分注释

--- spring-demo/src/main/java/com/pockyr/controller/EmpController.java
+++ spring-demo/src/main/java/com/pockyr/controller/EmpController.java
@@ -1,5 +1,6 @@
 package com.pockyr.controller;
 
+import com.pockyr.pojo.Employee;
 import com.pockyr.pojo.PageBean;
 import com.pockyr.pojo.Result;
 import com.pockyr.service.impl.EmpService;
@@ -13,36 +14,50 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@RequestMapping("/emps")
 public class EmpController {
     @Autowired
     private EmpService empService;
 
     /**
-     * 获取通过条件筛选出来的Employees数据中指定页数中的Employee数据和数据总数total, 包装为实体类PageBean, 传递到响应信息中
+     * 获取通过条件筛选出来的员工数据中指定页数中的员工数据和数据总数, 包装为实体类PageBean, 传递到响应信息中
      *
      * @param page     页数
      * @param pageSize 一页的数据量
      * @return 响应信息
      */
-    @GetMapping("/emps")
+    @GetMapping
     public Result page(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer pageSize,
                        String name, Short gender, @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
                        @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
         PageBean pageBean = empService.page(page, pageSize, name, gender, begin, end);
-        log.info("筛选employee, 筛选条件: namePart: {}, gender: {}, entryTimeBegin: {}, entryTimeEnd: {}, 显示页数: {}, 每页数量: {}", name, gender, begin, end, page, pageSize);
+        log.info("筛选员工数据, 筛选条件: namePart: {}, gender: {}, entryTimeBegin: {}, entryTimeEnd: {}, 显示页数: {}, 每页数量: {}", name, gender, begin, end, page, pageSize);
         return Result.success(pageBean);
     }
 
     /**
-     * 批量根据id删除employee数据
+     * 根据id批量删除员工数据
      *
      * @param ids id列表
      * @return 响应信息
      */
-    @DeleteMapping("/emps/{ids}")
+    @DeleteMapping("/{ids}")
     public Result delete(@PathVariable List<Integer> ids) {
         empService.delete(ids);
-        log.info("删除employee数据, 被删除的id: {}", ids);
+        log.info("删除员工数据, 被删除的id: {}", ids);
+        return Result.success();
+    }
+
+    /**
+     * 添加员工
+     *
+     * @param employee 包装了body中的员工数据的实体类实例
+     * @return 响应信息
+     */
+    @PostMapping
+    public Result add(@RequestBody Employee employee) {
+        empService.add(employee);
+        log.info("添加员工: {}", employee);
         return Result.success();
     }
 }
