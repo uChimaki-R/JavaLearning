971f5d165e5bfaea8a36428943a7f89c077f0652
Crestimes <1259864733@qq.com>
2024-06-18 22:13:08 +0800

    重命名方法: selectAll -> selectByConditions，添加作为筛选条件的参数，在xml文件中编写sql语句。重命名接口: EmpMapperInterface -> EmpMapper（因为mapper无需自己手写实现类，命名就简化掉Interface了）

--- spring-demo/src/main/java/com/pockyr/mapper/EmpMapper.java
+++ spring-demo/src/main/java/com/pockyr/mapper/EmpMapper.java
@@ -2,12 +2,12 @@ package com.pockyr.mapper;
 
 import com.pockyr.pojo.Employee;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Mapper
-public interface EmpMapperInterface {
+public interface EmpMapper {
 //    @Select("select count(*) from emp;")
 //    Long count();
 //
@@ -15,6 +15,6 @@ public interface EmpMapperInterface {
 //    List<Employee> page(Integer start, Integer pageSize);
 
     // 使用了PageHelper插件后只用使用普通的查询语句就行了，无需使用count和limit的sql语句
-    @Select("select * from emp")
-    List<Employee> selectAll();
+//    @Select("select * from emp") // 复杂的查询使用xml文件书写
+    List<Employee> selectByConditions(String namePart, Short gender, LocalDate begin, LocalDate end);
 }
