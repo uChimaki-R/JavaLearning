fc9aa24ef3418097a4fc31515f2cbbb096b21243
Crestimes <1259864733@qq.com>
2024-06-18 21:35:32 +0800

    使用PageHelper插件后只需要查询的sql语句

--- spring-demo/src/main/java/com/pockyr/mapper/EmpMapperInterface.java
+++ spring-demo/src/main/java/com/pockyr/mapper/EmpMapperInterface.java
@@ -8,9 +8,13 @@ import java.util.List;
 
 @Mapper
 public interface EmpMapperInterface {
-    @Select("select count(*) from emp;")
-    Long count();
+//    @Select("select count(*) from emp;")
+//    Long count();
+//
+//    @Select("select * from emp limit #{start}, #{pageSize}")
+//    List<Employee> page(Integer start, Integer pageSize);
 
-    @Select("select * from emp limit #{start}, #{pageSize}")
-    List<Employee> page(Integer start, Integer pageSize);
+    // 使用了PageHelper插件后只用使用普通的查询语句就行了，无需使用count和limit的sql语句
+    @Select("select * from emp")
+    List<Employee> selectAll();
 }
