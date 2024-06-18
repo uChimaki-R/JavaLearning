e0436a3a011595179f72ba41082cd6ffd31dfd7e
Crestimes <1259864733@qq.com>
2024-06-18 22:52:42 +0800

    添加方法add

--- spring-demo/src/main/java/com/pockyr/service/EmpServiceInterface.java
+++ spring-demo/src/main/java/com/pockyr/service/EmpServiceInterface.java
@@ -1,5 +1,6 @@
 package com.pockyr.service;
 
+import com.pockyr.pojo.Employee;
 import com.pockyr.pojo.PageBean;
 
 import java.time.LocalDate;
@@ -9,4 +10,6 @@ public interface EmpServiceInterface {
     PageBean page(Integer page, Integer pageSize, String namePart, Short gender, LocalDate begin, LocalDate end);
 
     void delete(List<Integer> ids);
+
+    void add(Employee employee);
 }
