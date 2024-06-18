1c78f2f0ba38c9149194668e4fe7147d9a1501a7
Crestimes <1259864733@qq.com>
2024-06-18 22:31:19 +0800

    添加处理删除员工的请求的方法，添加日志的记录和输出代码

--- spring-demo/src/main/java/com/pockyr/controller/EmpController.java
+++ spring-demo/src/main/java/com/pockyr/controller/EmpController.java
@@ -3,14 +3,15 @@ package com.pockyr.controller;
 import com.pockyr.pojo.PageBean;
 import com.pockyr.pojo.Result;
 import com.pockyr.service.impl.EmpService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
+@Slf4j
 @RestController
 public class EmpController {
     @Autowired
@@ -28,6 +29,20 @@ public class EmpController {
                        String name, Short gender, @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
                        @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
         PageBean pageBean = empService.page(page, pageSize, name, gender, begin, end);
+        log.info("筛选employee, 筛选条件: namePart: {}, gender: {}, entryTimeBegin: {}, entryTimeEnd: {}, 显示页数: {}, 每页数量: {}", name, gender, begin, end, page, pageSize);
         return Result.success(pageBean);
+    }
+
+    /**
+     * 批量根据id删除employee数据
+     *
+     * @param ids id列表
+     * @return 响应信息
+     */
+    @DeleteMapping("/emps/{ids}")
+    public Result delete(@PathVariable List<Integer> ids) {
+        empService.delete(ids);
+        log.info("删除employee数据, 被删除的id: {}", ids);
+        return Result.success();
     }
 }
