94894caa70ed7341377d2b5187080bcd7e50101a
Crestimes <1259864733@qq.com>
2024-06-19 21:08:01 +0800

    使用包含配置文件信息的bean类传递配置参数，实现软编码

--- spring-demo/src/main/java/com/pockyr/utils/QiNiuKodoUtils.java
+++ spring-demo/src/main/java/com/pockyr/utils/QiNiuKodoUtils.java
@@ -6,25 +6,32 @@ import com.qiniu.storage.Region;
 import com.qiniu.storage.UploadManager;
 import com.qiniu.util.Auth;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.UUID;
 
+@Component
 @Slf4j
-public class qiniuKodoUtils {
-    public static String uploadImage(MultipartFile image) {
+public class QiNiuKodoUtils {
+    // 使用配置文件传递参数，实现软编码
+    @Autowired
+    private QiNiuKodoProperties qiNiuKodoProperties;
+
+    public String uploadImage(MultipartFile image) {
         // 上传到云服务
         Configuration cfg = new Configuration(Region.huanan()); // 华南存储区域
         cfg.resumableUploadAPIVersion = Configuration.ResumableUploadAPIVersion.V2;// 指定分片上传版本
         UploadManager uploadManager = new UploadManager(cfg);
         // 配置信息
-        String accessKey = "*****";
-        String secretKey = "*****";
-        String bucket = "pockyr-public";
+        String accessKey = qiNiuKodoProperties.getAccessKey();
+        String secretKey = qiNiuKodoProperties.getSecretKey();
+        String bucket = qiNiuKodoProperties.getBucket();
         String filename = image.getOriginalFilename();
         // 使用uuid避免文件名重复
-        String key = "images/" + UUID.randomUUID() + filename;
+        String key = qiNiuKodoProperties.getFolder() + UUID.randomUUID() + filename;
         Auth auth = Auth.create(accessKey, secretKey);
         String upToken = auth.uploadToken(bucket);
         try {
@@ -35,6 +42,6 @@ public class qiniuKodoUtils {
             throw new RuntimeException(e);
         }
         // 拼装图片访问路径并返回
-        return "http://sfb62n5c3.hn-bkt.clouddn.com/" + key;
+        return qiNiuKodoProperties.getDomain() + key;
     }
 }
