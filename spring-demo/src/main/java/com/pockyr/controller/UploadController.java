30160cad06831a728c1abff7e00ae6ae3d9e4788
Crestimes <1259864733@qq.com>
2024-06-19 21:07:04 +0800

    工具类改为自动注入的成员变量，利于解耦

--- spring-demo/src/main/java/com/pockyr/controller/UploadController.java
+++ spring-demo/src/main/java/com/pockyr/controller/UploadController.java
@@ -1,8 +1,9 @@
 package com.pockyr.controller;
 
 import com.pockyr.pojo.Result;
-import com.pockyr.utils.qiniuKodoUtils;
+import com.pockyr.utils.QiNiuKodoUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,6 +11,8 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 @RestController
 public class UploadController {
+    @Autowired
+    private QiNiuKodoUtils qiNiuKodoUtils;
 
     /**
      * 处理上传图片文件的请求
@@ -18,7 +21,7 @@ public class UploadController {
      */
     @PostMapping("/upload")
     public Result upload(MultipartFile image){
-        String path = qiniuKodoUtils.uploadImage(image);
+        String path = qiNiuKodoUtils.uploadImage(image);
         log.info("文件上传, 文件名: {}, 访问的url: {}", image.getOriginalFilename(), path);
         return Result.success(path);
     }
