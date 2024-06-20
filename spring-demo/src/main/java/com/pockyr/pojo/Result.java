026f36a18e97725feb4fc08c5d1418000afbb701
Crestimes <1259864733@qq.com>
2024-06-20 15:20:26 +0800

    修正静态方法error

--- spring-demo/src/main/java/com/pockyr/pojo/Result.java
+++ spring-demo/src/main/java/com/pockyr/pojo/Result.java
@@ -20,7 +20,7 @@ public class Result {
         return new Result(1, "success", null);
     }
 
-    public static Result error() {
-        return new Result(0, "error", null);
+    public static Result error(String msg) {
+        return new Result(0, msg, null);
     }
 }
