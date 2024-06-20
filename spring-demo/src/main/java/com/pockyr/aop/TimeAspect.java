e649e1318d6ed9932c248beab331a532b9b20ce7
Crestimes <1259864733@qq.com>
2024-06-20 21:50:52 +0800

    将切入点表达式提取出来，提高代码的复用性

--- spring-demo/src/main/java/com/pockyr/aop/TimeAspect.java
+++ spring-demo/src/main/java/com/pockyr/aop/TimeAspect.java
@@ -4,13 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @Aspect // AOP类
 public class TimeAspect {
-    @Around("execution(* com.pockyr.service.*.*(..))") // 标记功能包装的方法范围(service中所有类和接口的所有方法) // 切入点表达式
+    @Pointcut("execution(* com.pockyr.service.*.*(..))") // 将切入点表达式提取出来，提高代码的复用性
+    public void pointCut() {} // 使用public，在其他AOP类中也可以使用，使用全类名.pointCut()
+
+    @Around("pointCut()") // 标记功能包装的方法范围(service中所有类和接口的所有方法) // 切入点表达式
     public Object recordTime(ProceedingJoinPoint joinPoint) throws Throwable {
         // 记录开始时间
         Long startTime = System.currentTimeMillis();
