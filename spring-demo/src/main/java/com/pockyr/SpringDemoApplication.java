c809a700e2dbe5a1390842562a13a7dfcbc69c50
Crestimes <1259864733@qq.com>
2024-06-20 15:43:43 +0800

    使用@ServletComponentScan注解开启Servlet组件支持

--- spring-demo/src/main/java/com/pockyr/SpringDemoApplication.java
+++ spring-demo/src/main/java/com/pockyr/SpringDemoApplication.java
@@ -2,7 +2,9 @@ package com.pockyr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 开启Servlet组件支持(因为spring集成的是自己的interceptor，所以需要手动打开扫描java的Servlet)
 @SpringBootApplication
 public class SpringDemoApplication {
 
