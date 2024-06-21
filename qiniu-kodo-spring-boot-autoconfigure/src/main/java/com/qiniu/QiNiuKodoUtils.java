c8709529e48abbe4bd4de4c4703c947d95364b18
Crestimes <1259864733@qq.com>
2024-06-21 20:36:40 +0800

    将提供对象存储相关功能的工具类提取到模块中，模块使用starter包装，以便使用自动注入

--- qiniu-kodo-spring-boot-autoconfigure/src/main/java/com/qiniu/QiNiuKodoUtils.java
+++ qiniu-kodo-spring-boot-autoconfigure/src/main/java/com/qiniu/QiNiuKodoUtils.java
@@ -1,24 +1,25 @@
-package com.pockyr.utils;
+package com.qiniu;
 
-import com.qiniu.http.Response;
 import com.qiniu.storage.Configuration;
 import com.qiniu.storage.Region;
 import com.qiniu.storage.UploadManager;
 import com.qiniu.util.Auth;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.UUID;
 
-@Component
-@Slf4j
 public class QiNiuKodoUtils {
     // 使用配置文件传递参数，实现软编码
-    @Autowired
     private QiNiuKodoProperties qiNiuKodoProperties;
+
+    public QiNiuKodoProperties getQiNiuKodoProperties() {
+        return qiNiuKodoProperties;
+    }
+
+    public void setQiNiuKodoProperties(QiNiuKodoProperties qiNiuKodoProperties) {
+        this.qiNiuKodoProperties = qiNiuKodoProperties;
+    }
 
     public String uploadImage(MultipartFile image) {
         // 上传到云服务
@@ -31,13 +32,11 @@ public class QiNiuKodoUtils {
         String bucket = qiNiuKodoProperties.getBucket();
         String filename = image.getOriginalFilename();
         // 使用uuid避免文件名重复
-        String key = qiNiuKodoProperties.getFolder() + UUID.randomUUID() + filename;
+        String key = qiNiuKodoProperties.getFolder() + UUID.randomUUID() + "_" +  filename;
         Auth auth = Auth.create(accessKey, secretKey);
         String upToken = auth.uploadToken(bucket);
         try {
-            Response response = uploadManager.put(image.getInputStream().readAllBytes(), key, upToken);
-            //解析上传成功的结果
-            log.info("response: {}", response.bodyString());
+            uploadManager.put(image.getInputStream().readAllBytes(), key, upToken);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
