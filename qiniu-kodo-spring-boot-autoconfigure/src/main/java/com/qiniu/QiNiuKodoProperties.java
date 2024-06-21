ab5cac453892647914b833b36ceaebe5c3859823
Crestimes <1259864733@qq.com>
2024-06-21 20:39:07 +0800

    autoconfigure模块中的配置信息实体类

---
+++ qiniu-kodo-spring-boot-autoconfigure/src/main/java/com/qiniu/QiNiuKodoProperties.java
@@ -0,0 +1,63 @@
+package com.qiniu;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "qiniu.kodo") // prefix中开头只能小写
+public class QiNiuKodoProperties {
+    private String accessKey;
+    private String secretKey;
+    private String bucket;
+    private String folder;
+    private String domain;
+
+    public QiNiuKodoProperties() {
+    }
+
+    public QiNiuKodoProperties(String accessKey, String secretKey, String bucket, String folder, String domain) {
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+        this.bucket = bucket;
+        this.folder = folder;
+        this.domain = domain;
+    }
+
+    public String getAccessKey() {
+        return accessKey;
+    }
+
+    public void setAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String getBucket() {
+        return bucket;
+    }
+
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getFolder() {
+        return folder;
+    }
+
+    public void setFolder(String folder) {
+        this.folder = folder;
+    }
+}
