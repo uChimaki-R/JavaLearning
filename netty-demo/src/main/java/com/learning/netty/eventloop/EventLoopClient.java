5a9c2c3c1ff5694b2d9dc34e54d88b5fc78174c3
Crestimes <1259864733@qq.com>
2024-10-17 23:28:53 +0800

    feat: 新增不断发送消息的代码逻辑。新增关闭客户端的代码逻辑。

--- netty-demo/src/main/java/com/learning/netty/eventloop/EventLoopClient.java
+++ netty-demo/src/main/java/com/learning/netty/eventloop/EventLoopClient.java
@@ -2,19 +2,24 @@ package com.learning.netty.eventloop;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
+import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetSocketAddress;
-import java.nio.charset.Charset;
+import java.util.Scanner;
 
+@Slf4j
 public class EventLoopClient {
     public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup();
         Channel channel = new Bootstrap()
                 .channel(NioSocketChannel.class)
-                .group(new NioEventLoopGroup())
+                .group(group)
                 .handler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
@@ -24,8 +29,31 @@ public class EventLoopClient {
                 .connect(new InetSocketAddress("127.0.0.1", 8080))
                 .sync()
                 .channel();
-        channel.writeAndFlush(Charset.defaultCharset().encode("Hello World"));
-        // 记得将断点类型改为Thread，因为使用的是线程池，发送消息的不是在当前线程，而断点默认是全局断点，会让发送消息的线程也停下，从而导致发不出去
-        System.out.println("breakpoint");
+
+        // 开启线程可以一直发送信息
+        new Thread(() -> {
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                String line = scanner.nextLine();
+                if ("quit".equalsIgnoreCase(line)) {
+                    break;
+                }
+                channel.writeAndFlush(line);
+            }
+            // 发送完关闭通道
+            channel.close();
+        }).start();
+
+        // 注册关闭后的逻辑
+        channel.closeFuture().addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                // 需要把事件循环组关闭，主进程才会结束
+                group.shutdownGracefully();
+                log.info("channel closed");
+            }
+        });
+
+        channel.closeFuture().sync();
     }
 }
