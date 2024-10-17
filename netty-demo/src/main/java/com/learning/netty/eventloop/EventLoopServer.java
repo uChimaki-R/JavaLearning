e6554f2b40cbf38ee0099c0aa92451d992d2279c
Crestimes <1259864733@qq.com>
2024-10-17 17:09:26 +0800

    feat: 新增简单的netty服务端

---
+++ netty-demo/src/main/java/com/learning/netty/eventloop/EventLoopServer.java
@@ -0,0 +1,35 @@
+package com.learning.netty.eventloop;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class EventLoopServer {
+    public static void main(String[] args) {
+        ServerBootstrap bootstrap = new ServerBootstrap()
+                .channel(NioServerSocketChannel.class)
+                .group(new NioEventLoopGroup())
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        nioSocketChannel.pipeline()
+                                .addLast(new StringDecoder())
+                                .addLast(new ChannelInboundHandlerAdapter() {
+                                    @Override
+                                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                        log.info("receive msg: {}", msg);
+                                        ctx.fireChannelRead(msg);
+                                    }
+                                });
+                    }
+                });
+        bootstrap.bind(8080);
+    }
+}
