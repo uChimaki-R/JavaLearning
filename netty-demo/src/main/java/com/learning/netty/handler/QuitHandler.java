659c498b95f63e84626d00e6b16ed6df49b7d822
Crestimes <1259864733@qq.com>
2024-10-18 15:40:05 +0800

    feat: 新增channel退出handler类，关注正常断开连接和异常断开连接的事件

---
+++ netty-demo/src/main/java/com/learning/netty/handler/QuitHandler.java
@@ -0,0 +1,18 @@
+package com.learning.netty.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class QuitHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        log.info("连接断开");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        log.warn("连接异常断开");
+    }
+}
