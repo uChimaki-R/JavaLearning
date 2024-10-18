65445b46da59b057c2df1fa6ca95d31b788486ef
Crestimes <1259864733@qq.com>
2024-10-18 20:52:51 +0800

    fix: 添加@Sharable注解

--- netty-demo/src/main/java/com/learning/netty/handler/LoginRequestMessageHandler.java
+++ netty-demo/src/main/java/com/learning/netty/handler/LoginRequestMessageHandler.java
@@ -2,11 +2,13 @@ package com.learning.netty.handler;
 
 import com.learning.netty.protocol.domain.LoginRequestMessage;
 import com.learning.netty.protocol.domain.LoginResponseMessage;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@ChannelHandler.Sharable
 public class LoginRequestMessageHandler extends SimpleChannelInboundHandler<LoginRequestMessage> {
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, LoginRequestMessage loginRequestMessage) throws Exception {
--- netty-demo/src/main/java/com/learning/netty/handler/QuitHandler.java
+++ netty-demo/src/main/java/com/learning/netty/handler/QuitHandler.java
@@ -1,10 +1,12 @@
 package com.learning.netty.handler;
 
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@ChannelHandler.Sharable
 public class QuitHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
