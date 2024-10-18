837f89974a93c2ead06eee99e790922c7b69dcee
Crestimes <1259864733@qq.com>
2024-10-18 14:51:17 +0800

    feat: 编写简单的处理登录请求的代码

--- netty-demo/src/main/java/com/learning/netty/eventloop/EventLoopServer.java
+++ netty-demo/src/main/java/com/learning/netty/eventloop/EventLoopServer.java
@@ -1,5 +1,8 @@
 package com.learning.netty.eventloop;
 
+import com.learning.netty.protocol.MessageCodec;
+import com.learning.netty.protocol.domain.LoginRequestMessage;
+import com.learning.netty.protocol.domain.LoginResponseMessage;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -7,12 +10,15 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class EventLoopServer {
     public static void main(String[] args) {
+        // 各channel可以公用的handler
+        MessageCodec MESSAGE_CODEC = new MessageCodec();
+
         ServerBootstrap bootstrap = new ServerBootstrap()
                 .channel(NioServerSocketChannel.class)
                 .group(new NioEventLoopGroup())
@@ -20,25 +26,19 @@ public class EventLoopServer {
                     @Override
                     protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
                         nioSocketChannel.pipeline()
-                                .addLast(new StringDecoder())
-                                .addLast(new ChannelInboundHandlerAdapter() {
+                                .addLast(new LengthFieldBasedFrameDecoder(1024, 12, 4))
+                                .addLast(MESSAGE_CODEC)
+                                .addLast(new ChannelInboundHandlerAdapter(){
                                     @Override
                                     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                        log.info("receive msg from {}: {}", ctx.channel().remoteAddress(), msg);
+                                        if (msg instanceof LoginRequestMessage) {
+                                            LoginRequestMessage loginRequestMessage = (LoginRequestMessage) msg;
+                                            log.info("接收到登录请求 {}", loginRequestMessage);
+                                            LoginResponseMessage loginResponseMessage = new LoginResponseMessage("zhangsan".equals(loginRequestMessage.getUsername()) && "123".equals(loginRequestMessage.getPassword()));
+                                            log.info("返回登录结果 {}", loginResponseMessage);
+                                            ctx.writeAndFlush(loginResponseMessage);
+                                        }
                                         super.channelRead(ctx, msg);
-                                    }
-
-                                    @Override
-                                    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-                                        // 连接的时候会触发
-                                        log.info("connect from {}", ctx.channel().remoteAddress());
-                                        super.channelActive(ctx);
-                                    }
-
-                                    @Override
-                                    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-                                        log.info("disconnect from {}", ctx.channel().remoteAddress());
-                                        super.channelInactive(ctx);
                                     }
                                 });
                     }
