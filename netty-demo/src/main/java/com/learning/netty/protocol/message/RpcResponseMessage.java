0b8295b81404cd17ff74fb043f77ad9542fb8654
Crestimes <1259864733@qq.com>
2024-10-19 16:19:01 +0800

    fix: 补充result属性，修正构造方法。

--- netty-demo/src/main/java/com/learning/netty/protocol/message/RpcResponseMessage.java
+++ netty-demo/src/main/java/com/learning/netty/protocol/message/RpcResponseMessage.java
@@ -5,10 +5,12 @@ import lombok.Data;
 @Data
 public class RpcResponseMessage extends Message{
     private final Boolean success;
+    private final Object result;
     private final String error;
-    public RpcResponseMessage(Integer sequenceId, Boolean success, String error) {
+    public RpcResponseMessage(Integer sequenceId, Boolean success, String error, Object result) {
         super(sequenceId, MessageType.RpcResponseMessage);
         this.success = success;
+        this.result = result;
         this.error = error;
     }
 }
