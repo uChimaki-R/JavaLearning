db66df798ca7844e2a919a75af46ad99826141c0
Crestimes <1259864733@qq.com>
2024-10-19 15:30:38 +0800

    feat: 新增信息类型枚举类

---
+++ netty-demo/src/main/java/com/learning/netty/protocol/message/MessageType.java
@@ -0,0 +1,9 @@
+package com.learning.netty.protocol.message;
+
+public enum MessageType {
+    LoginRequestMessage,
+    LoginResponseMessage,
+    PingMessage,
+    RpcRequestMessage,
+    RpcResponseMessage,
+}
