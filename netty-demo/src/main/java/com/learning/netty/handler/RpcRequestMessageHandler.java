f3eeff897adc7dbbd3e1d96cf70599ac448752c2
Crestimes <1259864733@qq.com>
2024-10-19 16:21:38 +0800

    feat: 新增rpc请求信息处理的handler类，从信息中获取接口和调用方法名，找到对应实现类的方法传入请求带来的参数执行方法获取结果，将结果发送回请求方。

---
+++ netty-demo/src/main/java/com/learning/netty/handler/RpcRequestMessageHandler.java
@@ -0,0 +1,34 @@
+package com.learning.netty.handler;
+
+import com.learning.netty.protocol.message.RpcRequestMessage;
+import com.learning.netty.protocol.message.RpcResponseMessage;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.lang.reflect.Method;
+import java.util.ServiceLoader;
+
+@ChannelHandler.Sharable
+public class RpcRequestMessageHandler extends SimpleChannelInboundHandler<RpcRequestMessage> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, RpcRequestMessage rpcRequestMessage) throws Exception {
+        // 找到所有实现类
+        ServiceLoader<?> impls = ServiceLoader.load(Class.forName(rpcRequestMessage.getInterfaceName()));
+        if (impls.iterator().hasNext()) {
+            try {
+                // 获取实现类
+                Object impl = impls.iterator().next();
+                // 获取方法
+                Method method = impl.getClass().getMethod(rpcRequestMessage.getMethodName(), rpcRequestMessage.getParameterTypes());
+                // 执行方法
+                Object result = method.invoke(impl, rpcRequestMessage.getParams());
+                channelHandlerContext.writeAndFlush(new RpcResponseMessage(rpcRequestMessage.getSequenceId(), true, null, result));
+            } catch (Exception e) {
+                channelHandlerContext.writeAndFlush(new RpcResponseMessage(rpcRequestMessage.getSequenceId(), false, e.getMessage() == null ? "函数调用出错" : e.getMessage(), null));
+            }
+        } else {
+            channelHandlerContext.writeAndFlush(new RpcResponseMessage(rpcRequestMessage.getSequenceId(), false, "没有找到实现类", null));
+        }
+    }
+}
