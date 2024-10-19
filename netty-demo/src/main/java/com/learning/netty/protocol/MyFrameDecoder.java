0b3890675b963ed11b98357ecd488aa04b19191c
Crestimes <1259864733@qq.com>
2024-10-19 13:21:30 +0800

    refactor: 修改构造方法。

--- netty-demo/src/main/java/com/learning/netty/protocol/MyFrameDecoder.java
+++ netty-demo/src/main/java/com/learning/netty/protocol/MyFrameDecoder.java
@@ -4,10 +4,6 @@ import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 
 public class MyFrameDecoder extends LengthFieldBasedFrameDecoder {
     public MyFrameDecoder() {
-        super(1024, 12, 4);
-    }
-
-    public MyFrameDecoder(int maxFrameLength, int lengthFieldOffset, int lengthFieldLength) {
-        super(maxFrameLength, lengthFieldOffset, lengthFieldLength);
+        super(1024, 12, 4, 0, 0);
     }
 }
