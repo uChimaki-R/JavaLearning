bacc8970aaacf928957819c4204c1bf2f46cce8a
Crestimes <1259864733@qq.com>
2024-10-19 16:25:32 +0800

    feat: 添加处理rpc请求信息的handler。 refactor: 注释掉心跳机制相关的代码段。 docs: 添加部分注释。

--- netty-demo/src/main/java/com/learning/netty/eventloop/EventLoopServer.java
+++ netty-demo/src/main/java/com/learning/netty/eventloop/EventLoopServer.java
@@ -2,19 +2,15 @@ package com.learning.netty.eventloop;
 
 import com.learning.netty.handler.LoginRequestMessageHandler;
 import com.learning.netty.handler.QuitHandler;
+import com.learning.netty.handler.RpcRequestMessageHandler;
 import com.learning.netty.protocol.MessageCodec;
 import com.learning.netty.protocol.MyFrameDecoder;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelDuplexHandler;
-import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LoggingHandler;
-import io.netty.handler.timeout.IdleState;
-import io.netty.handler.timeout.IdleStateEvent;
-import io.netty.handler.timeout.IdleStateHandler;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -24,6 +20,7 @@ public class EventLoopServer {
         MessageCodec MESSAGE_CODEC = new MessageCodec();
         LoginRequestMessageHandler LOGIN_REQUEST_HANDLER = new LoginRequestMessageHandler();
         QuitHandler QUIT_HANDLER = new QuitHandler();
+        RpcRequestMessageHandler RPC_REQUEST_HANDLER = new RpcRequestMessageHandler();
 
         ServerBootstrap bootstrap = new ServerBootstrap()
                 .channel(NioServerSocketChannel.class)
@@ -36,18 +33,22 @@ public class EventLoopServer {
                                 .addLast(new LoggingHandler())
                                 .addLast(MESSAGE_CODEC)
                                 .addLast(new LoggingHandler())
-                                .addLast(new IdleStateHandler(5, 0, 0))
-                                .addLast(new ChannelDuplexHandler() {
-                                    @Override
-                                    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-                                        IdleStateEvent event = (IdleStateEvent) evt;
-                                        if (IdleState.READER_IDLE.equals(event.state())) {
-                                            log.info("心跳信息间隔超过5秒, 断开连接");
-                                            ctx.channel().close();
-                                        }
-                                    }
-                                })
+//                                .addLast(new IdleStateHandler(5, 0, 0))
+//                                .addLast(new ChannelDuplexHandler() {
+//                                    @Override
+//                                    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+//                                        IdleStateEvent event = (IdleStateEvent) evt;
+//                                        if (IdleState.READER_IDLE.equals(event.state())) {
+//                                            log.info("心跳信息间隔超过5秒, 断开连接");
+//                                            ctx.channel().close();
+//                                        }
+//                                    }
+//                                })
+                                // rpc请求信息处理
+                                .addLast(RPC_REQUEST_HANDLER)
+                                // 登录请求信息处理
                                 .addLast(LOGIN_REQUEST_HANDLER)
+                                // 断连处理
                                 .addLast(QUIT_HANDLER);
                     }
                 });
