b72969df2aa9d8c6b868dc50f2636c05c1c2880f
Crestimes <1259864733@qq.com>
2025-03-21 17:08:54 +0800

    feat: 新增用于测试shardingsphere分库分表的课程实体类

---
+++ skill-stacks/src/main/java/shardingJDBC/standard/po/Course.java
@@ -0,0 +1,16 @@
+package shardingJDBC.standard.po;
+
+import lombok.Data;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-21
+ * @Description: 用于测试的课程实体类
+ * @Version: 1.0
+ */
+@Data
+public class Course {
+    private Long cid;
+    private String cname;
+    private Integer cStatus;
+}
