a171ffe9dc63089729c0710de6e0971f743113d6
Crestimes <1259864733@qq.com>
2025-03-21 16:51:04 +0800

    feat: 添加启动类。 test: 在admin服务器上调用任务执行，测试通过。

---
+++ skill-stacks/src/main/java/xxlJob/XxlJobTestApplication.java
@@ -0,0 +1,17 @@
+package xxlJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-20
+ * @Description: spring框架下的xxl-job使用测试
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class XxlJobTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobTestApplication.class, args);
+    }
+}
