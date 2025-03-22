d320a3506ea2ccba093eeab9e82327e94628f2a8
Crestimes <1259864733@qq.com>
2025-03-22 14:27:38 +0800

    feat: 新增CLASS_BASED分片测试启动类

---
+++ skill-stacks/src/main/java/shardingJDBC/classBased/ClassBasedApplication.java
@@ -0,0 +1,17 @@
+package shardingJDBC.classBased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-22
+ * @Description: CLASS_BASED分片测试
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class ClassBasedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClassBasedApplication.class, args);
+    }
+}
