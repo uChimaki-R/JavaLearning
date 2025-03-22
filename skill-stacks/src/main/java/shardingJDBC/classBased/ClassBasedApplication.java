797af78fe08c7c20a86ad7c291a6ca0372ab9ae4
Crestimes <1259864733@qq.com>
2025-03-22 15:29:11 +0800

    fix: 补充mapper包扫描路径

--- skill-stacks/src/main/java/shardingJDBC/classBased/ClassBasedApplication.java
+++ skill-stacks/src/main/java/shardingJDBC/classBased/ClassBasedApplication.java
@@ -1,5 +1,6 @@
 package shardingJDBC.classBased;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version: 1.0
  */
 @SpringBootApplication
+@MapperScan("shardingJDBC.mapper")
 public class ClassBasedApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClassBasedApplication.class, args);
