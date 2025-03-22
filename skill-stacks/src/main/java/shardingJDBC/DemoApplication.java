d864838847e480252db1ec982b812d09697800da
Crestimes <1259864733@qq.com>
2025-03-22 21:30:31 +0800

    feat: 添加综合测试的spring启动类

---
+++ skill-stacks/src/main/java/shardingJDBC/DemoApplication.java
@@ -0,0 +1,17 @@
+package shardingJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-22
+ * @Description: 综合测试，启动类
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
