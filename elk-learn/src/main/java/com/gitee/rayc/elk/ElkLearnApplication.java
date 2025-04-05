7393c0caaec05116f6d0e1696b8aaf7292f41e9f
Ray-C <1259864733@qq.com>
2025-04-05 22:01:32 +0800

    feat: 新增启动类，测试日志输出是否能够通过logstash存储到es中，测试通过

---
+++ elk-learn/src/main/java/com/gitee/rayc/elk/ElkLearnApplication.java
@@ -0,0 +1,17 @@
+package com.gitee.rayc.elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-27
+ * @Description: elk测试启动类
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class ElkLearnApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElkLearnApplication.class, args);
+    }
+}
