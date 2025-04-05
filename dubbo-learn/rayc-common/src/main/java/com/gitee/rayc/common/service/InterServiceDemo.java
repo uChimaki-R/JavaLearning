5b3cd1c7ddaacac5e2bec71e54ef9e4fac6be378
Ray-C <1259864733@qq.com>
2025-04-05 22:03:58 +0800

    feat: 新增远程调用service接口demo

---
+++ dubbo-learn/rayc-common/src/main/java/com/gitee/rayc/common/service/InterServiceDemo.java
@@ -0,0 +1,11 @@
+package com.gitee.rayc.common.service;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-25
+ * @Description: 远程调用service接口demo
+ * @Version: 1.0
+ */
+public interface InterServiceDemo {
+    String sayHello(String name);
+}
