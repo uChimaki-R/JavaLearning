b30bdfe1c28be634c78bbe10e3f6a3e2f6150cc5
Ray-C <1259864733@qq.com>
2025-04-08 21:48:56 +0800

    feat: 添加启动类

---
+++ curator-learn/src/main/java/com/gitee/rayc/curatorL/dynamicProp/CuratorLearnApplication.java
@@ -0,0 +1,17 @@
+package com.gitee.rayc.curatorL.dynamicProp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-27
+ * @Description: zookeeper使用curator实现动态配置测试
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class CuratorLearnApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CuratorLearnApplication.class, args);
+    }
+}
